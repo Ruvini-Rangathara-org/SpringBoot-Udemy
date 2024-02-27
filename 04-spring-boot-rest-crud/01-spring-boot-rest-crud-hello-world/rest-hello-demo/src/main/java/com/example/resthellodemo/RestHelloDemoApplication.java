8a0fde903e7fb2561c1f2d5c05038d8ec09c9586
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-02-28 00:23:44 +0530

    hello rest api

---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud-hello-world/rest-hello-demo/src/main/java/com/example/resthellodemo/RestHelloDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.resthellodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestHelloDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestHelloDemoApplication.class, args);
+	}
+
+}
---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud-hello-world/rest-hello-demo/src/test/java/com/example/resthellodemo/RestHelloDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resthellodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestHelloDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
