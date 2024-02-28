479322e8f42bcbfe8edf787900984bf08ad4b074
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-02-28 12:55:36 +0530

    employee crud-delete

--- 04-spring-boot-rest-crud/09-spring-boot-rest-crud-employee-dao-find-add-update-delete/crud-employee-demo/src/main/java/com/example/crudemployeedemo/rest/EmployeeController.java
+++ 04-spring-boot-rest-crud/09-spring-boot-rest-crud-employee-dao-find-add-update-delete/crud-employee-demo/src/main/java/com/example/crudemployeedemo/rest/EmployeeController.java
@@ -46,4 +46,14 @@ public class EmployeeController {
         Employee save = employeeService.save(employee);
         return save;
     }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        employeeService.deleteById(employeeId);
+        return "Deleted employee id - " + employeeId;
+    }
 }
