7e8c67bd7fbe77ac1286992dfc911b94c5627c83
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-02-28 16:16:34 +0530

    employee data rest config

---
+++ 04-spring-boot-rest-crud/12-spring-boot-rest-crud-employee-with-spring-data-rest-configs-and-sorting/data-rest-config-demo/src/main/java/com/example/datarestconfigdemo/DataRestConfigDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.datarestconfigdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRestConfigDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataRestConfigDemoApplication.class, args);
+	}
+
+}
---
+++ 04-spring-boot-rest-crud/12-spring-boot-rest-crud-employee-with-spring-data-rest-configs-and-sorting/data-rest-config-demo/src/main/java/com/example/datarestconfigdemo/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.example.datarestconfigdemo.dao;
+
+
+import com.example.datarestconfigdemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
