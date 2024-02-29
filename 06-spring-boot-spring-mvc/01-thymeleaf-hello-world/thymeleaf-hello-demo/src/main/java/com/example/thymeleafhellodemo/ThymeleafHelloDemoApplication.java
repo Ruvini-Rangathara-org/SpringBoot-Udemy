b9823bfdb8e876c770b20b3c6f3d100fffca3bef
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-02-29 13:20:30 +0530

    thymeleaf hello with css

---
+++ 06-spring-boot-spring-mvc/01-thymeleaf-hello-world/thymeleaf-hello-demo/src/main/java/com/example/thymeleafhellodemo/ThymeleafHelloDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafhellodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafHelloDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafHelloDemoApplication.class, args);
+	}
+
+}
---
+++ 06-spring-boot-spring-mvc/01-thymeleaf-hello-world/thymeleaf-hello-demo/src/test/java/com/example/thymeleafhellodemo/ThymeleafHelloDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafhellodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafHelloDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
