8029b98660c203031ab67564fbf83841d54695c2
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-03-04 12:09:55 +0530

    data binding with text fields

---
+++ 06-spring-boot-spring-mvc/06-form-data-binding-textfields/text-field-demo/src/main/java/com/example/textfielddemo/TextFieldDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.textfielddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextFieldDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextFieldDemoApplication.class, args);
+	}
+
+}
---
+++ 06-spring-boot-spring-mvc/06-form-data-binding-textfields/text-field-demo/src/test/java/com/example/textfielddemo/TextFieldDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.textfielddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextFieldDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
