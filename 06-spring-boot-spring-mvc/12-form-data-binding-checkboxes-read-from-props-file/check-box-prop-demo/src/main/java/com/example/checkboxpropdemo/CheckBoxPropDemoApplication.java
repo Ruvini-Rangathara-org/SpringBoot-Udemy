b4524c9d1b6f8517d1a5b260198f26b873c90ac3
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-03-07 21:41:44 +0530

    check box with props

---
+++ 06-spring-boot-spring-mvc/12-form-data-binding-checkboxes-read-from-props-file/check-box-prop-demo/src/main/java/com/example/checkboxpropdemo/CheckBoxPropDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.checkboxpropdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckBoxPropDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckBoxPropDemoApplication.class, args);
+	}
+
+}
---
+++ 06-spring-boot-spring-mvc/12-form-data-binding-checkboxes-read-from-props-file/check-box-prop-demo/src/test/java/com/example/checkboxpropdemo/CheckBoxPropDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkboxpropdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckBoxPropDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
