aa34e034173ff432569f7f6d6407f187a95061d8
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-03-11 09:41:18 +0530

    validation basic

---
+++ 06-spring-boot-spring-mvc/13-validationdemo-required-fields/validation-demo/src/main/java/com/example/validationdemo/Customer.java
@@ -0,0 +1,20 @@
+package com.example.validationdemo;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Customer {
+
+    private String firstName;
+
+    @NotNull(message="is required")
+    @Size(min=1, message="is required")
+    private String lastName = "";
+
+}
