89aa4cf89d9b0c7301265f7a23675805cd53b74c
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-03-11 14:10:35 +0530

    handle string for integer field

--- 06-spring-boot-spring-mvc/13-validationdemo-required-fields/validation-demo/src/main/java/com/example/validationdemo/Customer.java
+++ 06-spring-boot-spring-mvc/13-validationdemo-required-fields/validation-demo/src/main/java/com/example/validationdemo/Customer.java
@@ -1,9 +1,6 @@
 package com.example.validationdemo;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,8 +16,12 @@ public class Customer {
     @Size(min=1, message="is required")
     private String lastName = "";
 
+    @NotNull(message="is required")
     @Min(value=0, message="must be greater than or equal to zero")
     @Max(value=10, message="must be less than or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
 
+    @NotNull(message="is required")
+    @Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+    private String postalCode;
 }
