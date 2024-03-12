b3d20ae250bc729cfe241f4da77bdc814f8f0246
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-03-12 21:54:18 +0530

    mvc delete emp

---
+++ 07-spring-boot-spring-mvc-crud/04-spring-mvc-employee-delete/mvc-delete-demo/src/main/java/com/example/mvcdeletedemo/MvcDeleteDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvcdeletedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcDeleteDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcDeleteDemoApplication.class, args);
+	}
+
+}
---
+++ 07-spring-boot-spring-mvc-crud/04-spring-mvc-employee-delete/mvc-delete-demo/src/main/java/com/example/mvcdeletedemo/controller/EmployeeController.java
@@ -0,0 +1,55 @@
+package com.example.mvcdeletedemo.controller;
+
+
+import com.example.mvcdeletedemo.entity.Employee;
+import com.example.mvcdeletedemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
+    }
+
+    // add mapping for GET /employees
+    @GetMapping("/list")
+    public String listEmployees(Model theModel) {
+        List<Employee> theEmployees = employeeService.findAll();
+        theModel.addAttribute("employees", theEmployees);
+        return "employee/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+        Employee theEmployee = new Employee();
+        theModel.addAttribute("employee", theEmployee);
+        return "employee/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+        employeeService.save(theEmployee);
+        return "redirect:/employees/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+        Employee theEmployee = employeeService.findById(theId);
+        theModel.addAttribute("employee", theEmployee);
+        return "employee/employee-form";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int theId) {
+        employeeService.deleteById(theId);
+        return "redirect:/employees/list";
+    }
+}
