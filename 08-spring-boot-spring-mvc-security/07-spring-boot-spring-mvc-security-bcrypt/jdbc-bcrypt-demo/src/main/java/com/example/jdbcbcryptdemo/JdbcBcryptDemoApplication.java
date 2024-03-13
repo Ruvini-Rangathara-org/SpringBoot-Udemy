cab684121c70bcca59466e53fb0c63fce1a5e9b0
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-03-13 14:29:07 +0530

    sec with jdbc bcrypt

---
+++ 08-spring-boot-spring-mvc-security/07-spring-boot-spring-mvc-security-bcrypt/jdbc-bcrypt-demo/src/main/java/com/example/jdbcbcryptdemo/JdbcBcryptDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbcbcryptdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcBcryptDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcBcryptDemoApplication.class, args);
+	}
+
+}
