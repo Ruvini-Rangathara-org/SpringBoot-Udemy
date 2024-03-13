7a9e9f6fe8113bb1ebd17464494c262d00ceb0fd
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-03-13 09:54:24 +0530

    mvc custom login

---
+++ 08-spring-boot-spring-mvc-security/02-spring-boot-spring-mvc-security-custom-login-plain/custom-login-form-demo/src/main/java/com/example/customloginformdemo/CustomLoginFormDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.customloginformdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomLoginFormDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomLoginFormDemoApplication.class, args);
+	}
+
+}
