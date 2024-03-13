92e2027e7b5863b52e095534251341a177e85eae
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-03-13 13:05:08 +0530

    display content base on role

---
+++ 08-spring-boot-spring-mvc-security/05-spring-boot-spring-mvc-display-content-on-role/display-content-role-base-demo/src/main/java/com/example/displaycontentrolebasedemo/DisplayContentRoleBaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.displaycontentrolebasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisplayContentRoleBaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisplayContentRoleBaseDemoApplication.class, args);
+	}
+
+}
