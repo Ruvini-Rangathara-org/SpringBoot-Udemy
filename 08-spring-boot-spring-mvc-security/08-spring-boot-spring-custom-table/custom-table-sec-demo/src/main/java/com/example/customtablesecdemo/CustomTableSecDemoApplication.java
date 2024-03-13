9caae4ca3319a241bd2b2d238045756fe28031b1
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-03-13 14:42:58 +0530

    sec with custom table

---
+++ 08-spring-boot-spring-mvc-security/08-spring-boot-spring-custom-table/custom-table-sec-demo/src/main/java/com/example/customtablesecdemo/CustomTableSecDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.customtablesecdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomTableSecDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomTableSecDemoApplication.class, args);
+	}
+
+}
---
+++ 08-spring-boot-spring-mvc-security/08-spring-boot-spring-custom-table/custom-table-sec-demo/src/test/java/com/example/customtablesecdemo/CustomTableSecDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customtablesecdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomTableSecDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
