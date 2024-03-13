24e15d5c2a132a0771d1ff89396647eba445da05
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-03-13 12:52:08 +0530

    restrict on url

---
+++ 08-spring-boot-spring-mvc-security/04-spring-boot-spring-mvc-restrict-url-base-on-role/restrict-on-url-demo/src/main/java/com/example/restrictonurldemo/RestrictOnUrlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.restrictonurldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestrictOnUrlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestrictOnUrlDemoApplication.class, args);
+	}
+
+}
---
+++ 08-spring-boot-spring-mvc-security/04-spring-boot-spring-mvc-restrict-url-base-on-role/restrict-on-url-demo/src/test/java/com/example/restrictonurldemo/RestrictOnUrlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restrictonurldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestrictOnUrlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
