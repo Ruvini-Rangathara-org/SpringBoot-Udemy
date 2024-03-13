c18f8b6c0a8f0f3d574a54c88187c64a7b931780
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-03-13 12:14:11 +0530

    display user details

---
+++ 08-spring-boot-spring-mvc-security/03-spring-boot-spring-mvc-security-logout/logout-demo/src/test/java/com/example/logoutdemo/LogoutDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logoutdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogoutDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
