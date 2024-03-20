eab49329bfd48a883bfb215d647c157c0a752784
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-03-20 14:33:37 +0530

    aop @Around add exception

--- 10-spring-boot-aop/10-spring-boot-aop-demo-around-advice/around-demo/src/main/java/com/example/arounddemo/AroundDemoApplication.java
+++ 10-spring-boot-aop/10-spring-boot-aop-demo-around-advice/around-demo/src/main/java/com/example/arounddemo/AroundDemoApplication.java
@@ -18,13 +18,32 @@ public class AroundDemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, TrafficFortuneService theTrafficFortuneService) {
 		return runner -> {
-//			demoTheAfterReturningAdvice(theAccountDAO);
-//			demoTheAfterThrowingAdvice(theAccountDAO);
-//			demoTheAfterAdvice(theAccountDAO);
-			demoTheAroundAdvice(theTrafficFortuneService);
 
+//			demoTheAroundAdvice(theTrafficFortuneService);
+//			demoTheAroundAdviceHandleException(theTrafficFortuneService);
+
+			demoTheAroundAdviceRethrowException(theTrafficFortuneService);
 		};
 	}
+
+	private void demoTheAroundAdviceRethrowException(TrafficFortuneService theTrafficFortuneService) {
+		System.out.println("\nMain Program: demoTheAroundAdviceRethrowException");
+		System.out.println("Calling getFortune()");
+		boolean tripWire = true;
+		String data = theTrafficFortuneService.getFortune(tripWire);
+		System.out.println("\nMy fortune is: " + data);
+		System.out.println("Finished");
+	}
+
+	private void demoTheAroundAdviceHandleException(TrafficFortuneService theTrafficFortuneService) {
+		System.out.println("\nMain Program: demoTheAroundAdviceHandleException");
+		System.out.println("Calling getFortune()");
+		boolean tripWire = true;
+		String data = theTrafficFortuneService.getFortune(tripWire);
+		System.out.println("\nMy fortune is: " + data);
+		System.out.println("Finished");
+	}
+
 
 	private void demoTheAroundAdvice(TrafficFortuneService theTrafficFortuneService) {
 		System.out.println("\nMain Program: demoTheAroundAdvice");
@@ -32,53 +51,5 @@ public class AroundDemoApplication {
 		String data = theTrafficFortuneService.getFortune();
 		System.out.println("\nMy fortune is: " + data);
 		System.out.println("Finished");
-	}
-
-	private void demoTheAfterAdvice(AccountDAO theAccountDAO) {
-		// call method to find the accounts
-		List<Account> theAccounts = null;
-		try {
-			// add a boolean flag to simulate exceptions
-			boolean tripWire = false;
-			theAccounts = theAccountDAO.findAccounts(tripWire);
-		}
-		catch (Exception exc) {
-			System.out.println("\n\nMain Program: ... caught exception: " + exc);
-		}
-		// display the accounts
-		System.out.println("\n\nMain Program: demoTheAfterAdvice");
-		System.out.println("----");
-		System.out.println(theAccounts);
-		System.out.println("\n");
-	}
-
-	private void demoTheAfterThrowingAdvice(AccountDAO theAccountDAO) {
-		// call method to find the accounts
-		List<Account> theAccounts = null;
-		try {
-			// add a boolean flag to simulate exceptions
-			boolean tripWire = true;
-			theAccounts = theAccountDAO.findAccounts(tripWire);
-		}
-		catch (Exception exc) {
-			System.out.println("\n\nMain Program: ... caught exception: " + exc);
-		}
-		// display the accounts
-		System.out.println("\n\nMain Program: demoTheAfterThrowingAdvice");
-		System.out.println("----");
-		System.out.println(theAccounts);
-		System.out.println("\n");
-	}
-
-	private void demoTheAfterReturningAdvice(AccountDAO theAccountDAO) {
-		// call method to find the accounts
-		List<Account> theAccounts = theAccountDAO.findAccounts();
-
-		// display the accounts
-		System.out.println("\n\n\nMain Program: demoTheAfterReturningAdvice");
-		System.out.println("----");
-
-		System.out.println(theAccounts);
-		System.out.println("\n");
 	}
 }
--- 10-spring-boot-aop/10-spring-boot-aop-demo-around-advice/around-demo/src/main/java/com/example/arounddemo/service/TrafficFortuneServiceImpl.java
+++ 10-spring-boot-aop/10-spring-boot-aop-demo-around-advice/around-demo/src/main/java/com/example/arounddemo/service/TrafficFortuneServiceImpl.java
@@ -9,16 +9,21 @@ public class TrafficFortuneServiceImpl implements TrafficFortuneService {
 
     @Override
     public String getFortune() {
-
         // simulate a delay
-
         try {
             TimeUnit.SECONDS.sleep(5);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
         // return a fortune
         return "Expect heavy traffic this morning";
+    }
+
+    @Override
+    public String getFortune(boolean tripWire) {
+        if (tripWire) {
+            throw new RuntimeException("Major accident! Highway is closed!");
+        }
+        return getFortune();
     }
 }
